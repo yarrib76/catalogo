36bb9424b34db8c6d9416fdd85c5f7ed19c12c8f
Yamil <yarrib76@gmail.com>
2016-04-10 11:27:24 -0300

    EnviarClientes

---
+++ app/Http/Controllers/Api/EnviarClientesController.php
@@ -0,0 +1,20 @@
+<?php namespace Catalogos\Http\Controllers\Api;
+
+use Catalogos\Http\Requests;
+use Catalogos\Http\Controllers\Controller;
+
+use Catalogos\Models\ClientesInternos;
+use Catalogos\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Response;
+
+class EnviarClientesController extends Controller {
+
+    public function clientes(){
+        $cliente_id = User::where('email', Input::get('email'))->get()[0]->
+        load('miembrosDe')['miembrosDe'][0]->cliente_id;
+        $clientesInternos = ClientesInternos::where('cliente_id', $cliente_id)->get();
+        return Response::json($clientesInternos);
+    }
+}
